7a105528d7e4cff24a5d6bb4a77c3f081c8462e7
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-11-16 22:07:05 +0000

    Updates

--- lvl_1_ft_printf/srcs/prt_ptr.c
+++ lvl_1_ft_printf/srcs/prt_ptr.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 18:01:49 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/11/15 21:17:26 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/11/16 21:55:09 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	prt_ptr(void *ptr_addr)
 {
 	if (ptr_addr == 0)
-		return (write(1, "0x0", 3));
+		return (write(1, "(nil)", 5));
 	write(1, "0x", 2);
 	return (prt_hexa((unsigned long int)ptr_addr, false) + 2);
 }
